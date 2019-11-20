386e7ed2c6d83d05e7612b2f4e4e087a55830817
Rafaela <rafalucy3@gmail.com>
2019-11-20 19:50:01 +0000

    Add setWeatherData function and selector from index file to script file

--- public/script.js
+++ public/script.js
@@ -19,3 +19,22 @@ searchBox.addListener('places_changed', () => {
     setWeatherData(data, place.formatted_address)
   })
 })
+
+const icon = new Skycons({ color: '#222' })
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temperatureElement = document.querySelector('[data-temperature]')
+const precipitationElement = document.querySelector('[data-precipitation]')
+const windElement = document.querySelector('[data-wind]')
+icon.set('icon', 'clear-day')
+icon.play()
+
+function setWeatherData(data, place) {
+  locationElement.textContent = place
+  statusElement.textContent = data.summary
+  temperatureElement.textContent = data.temperature
+  precipitationElement.textContent = `${data.precipProbability * 100}%`
+  windElement.textContent = data.windSpeed
+  icon.set('icon', data.icon)
+  icon.play()
+}
